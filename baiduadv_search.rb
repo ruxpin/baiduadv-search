ce5386e0fa6da3e02a737d229fef3b2f9cb410be
luo xiaobin <ruxsfi@gmail.com>
2013-03-15 01:14:58 +0800

    improve performance by  keep reusing one browser,  refactor almost all methods, add debug mode option,
    Now the browser will not show during the run time , but it will flash at
    the start.

--- baiduadv_search.rb
+++ baiduadv_search.rb
@@ -6,98 +6,128 @@ require 'rchardet19'
 require 'watir'
 
 class Graper
-  attr_accessor :grap_links, :new_links, :keyword_list, :options
+  attr_accessor :keyword_list, :options, :browser
   
   def initialize(options={})
-    @grap_links = []
-    @new_links = []
     @keyword_list = []
     @options = options
-    @options.delete('keywords').each do |k|
-      @keyword_list << k[1]
+    fulfill_options
+    initialize_browser
+    write_html_meta
+  end
+
+  def fulfill_options
+    options.delete('keywords').each do |k|
+      keyword_list << k[1]
     end
-    @options['general'].merge! ({'rn' => '100'}) unless @options['general'].has_key?('rn')
-    @options['general'].merge! ({'totalpn' => '100'}) unless @options['general'].has_key?('totalpn')
+    options['general'].merge! ({'interval' => '1'}) unless options['general'].has_key?('interval')
+    options['general'].merge! ({'rn' => '100'}) unless options['general'].has_key?('rn')
+    options['general'].merge! ({'totalpn' => '100'}) unless options['general'].has_key?('totalpn')
+    if options['general'].has_key?('debug') &&  options['general']['debug'] == '1'
+      options['general'].merge! ({'debug' => true })
+    else
+      options['general'].merge! ({'debug' => false })
+    end
     %w[lm site].each do |a|
-      @options['general'].merge! ({a => ''}) unless @options['general'].has_key?(a)
+      options['general'].merge! ({a => ''}) unless options['general'].has_key?(a)
     end
+    puts "\n  关键词如下： \n\n  #{keyword_list}\n\n  options如下：\n\n  #{options}" if options['general']['debug']
+  end
+
+  def initialize_browser
+    @browser = Watir::Browser.new
+    @browser.visible = false unless options['general']['debug']
+  end
+
+  def write_html_meta
     File.open("new_pages.html","w") do |f|
       f.puts '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'
     end
   end
-  
+
   def get_baidu
-    puts ""
     put_separator
     keyword_list.each do |keyword|
-      grap_links=[]
       options.each do |k,v|
-        v['rn'] = options['general']['rn'] unless v['rn']
-        v['site'] = options['general']['site'] unless v['site']
-        v['lm'] = options['general']['lm'] unless v['lm']
-        v['totalpn'] = options['general']['totalpn'] unless v['totalpn']
+        v = fulfill_local_options(v)
         puts "  获取关键词【#{keyword}】在\<#{k}\>设置的前#{v['totalpn']}个搜索结果.."
         grap_baidu_links(keyword, k, v)
         save_new_links_to_db(keyword,k)
-        sleep 1
       end
     end
+    browser.close
+  end
+
+  def fulfill_local_options(v)
+    v['rn'] = options['general']['rn'] unless v['rn']
+    v['site'] = options['general']['site'] unless v['site']
+    v['lm'] = options['general']['lm'] unless v['lm']
+    v['totalpn'] = options['general']['totalpn'] unless v['totalpn']
+    v['interval'] = options['general']['interval'] unless v['interval']
+    v
   end
 
   def grap_baidu_links(keyword,k,v)
     pn = 0
+    @grap_links = []
     link_reg = /<H3 class=t>.*?<\/H3>/
     while pn < v['totalpn'].to_i
-      site="http://www.baidu.com/s?q1=#{keyword}&q2=&q3=&q4=&rn=#{v['rn']}&lm=#{v['lm']}&ct=0&ft=&q5=&q6=#{v['site']}&pn=#{pn}&tn=baiduadv"
-      $HIDE_Browser =true
-      browser = Watir::Browser.new
+      site = "http://www.baidu.com/s?q1=#{keyword}&q2=&q3=&q4=&rn=#{v['rn']}&lm=#{v['lm']}&ct=0&ft=&q5=&q6=#{v['site']}&pn=#{pn}&tn=baiduadv"
       browser.goto site
       browser.html.each_line do |line|
         if line.scan(link_reg)[0]
-          grap_links << line.scan(link_reg)
+          @grap_links << line.scan(link_reg)
         end
       end
-      browser.close
       pn += v['rn'].to_i
-      sleep 2
+      sleep v['interval'].to_i
     end
   end
 
+#百度搜索每个结果的url都带有唯一的hash值，只需要将获得的hash值取出并与数据库已有的hash值比对即可知道是不是没出现过的新结果
+#新结果的url_hash和title将写入数据库中
   def save_new_links_to_db(keyword,profile)
     puts "  在获取的搜索结果中查找新的内容.."
     url_reg = /href=\".*?\"/
-    db = SQLite3::Database.open "pagesHub.db"
-    new_links=[]
-    grap_links.each do |set|
-      set.each do |line|
-        if url = (line.scan url_reg)[0]
-          url.gsub!("href=","").gsub!("\"","").chomp!
-          title = line.gsub(/<.*?>/,"").gsub("\'",'').chomp
-        end
-        url_hash = url.sub(/^http:\/\/www.baidu.com\/link\?url=..../,"")
-        stm = db.prepare "select * from pages where url_hash=\'#{url_hash}\' and title=\'#{title}\'"
-        found = false
-        rs = stm.execute
-        found = true if rs.next
-        stm.close
-        unless found
-          db.execute "insert into pages(url,title,url_hash) values(\'#{url}\',\'#{title}\',\'#{url_hash}\')"
-          new_links << line.gsub("H3","H5")
+    begin
+      db = SQLite3::Database.open "pagesHub.db"
+      new_links=[]
+      @grap_links.each do |set|
+        set.each do |line|
+          if url = (line.scan url_reg)[0]
+            url.gsub!("href=","").gsub!("\"","").chomp!
+            title = line.gsub(/<.*?>/,"").gsub("\'",'').chomp
+          end
+          url_hash = url.sub(/^http:\/\/www.baidu.com\/link\?url=..../,"")
+          stm = db.prepare "select * from pages where url_hash=\'#{url_hash}\' and title=\'#{title}\'"
+          found = false
+          rs = stm.execute
+          found = true if rs.next
+          stm.close
+          unless found
+            puts "\nrun sql: insert into pages(url,title,url_hash) values(\'#{url}\',\'#{title}\',\'#{url_hash}\')" if options['general']['debug']
+            db.execute "insert into pages(url,title,url_hash) values(\'#{url}\',\'#{title}\',\'#{url_hash}\')"
+            new_links << line.gsub("H3","H5")
+          end
         end
       end
-    end
-    if !new_links.empty?
-      File.open("new_pages.html","a") do |f|
-        f.puts '<p>【'+keyword+"】在 "+profile+" 设置的搜索结果如下："+'</p>'
-        f.puts new_links
-        f.puts '<p>----- ----- ----- ----- ----- -----</p>'
+      if !new_links.empty?
+        File.open("new_pages.html","a") do |f|
+          f.puts '<p>【'+keyword+"】在 "+profile+" 设置的新搜索结果如下："+'</p>'
+          f.puts new_links
+          f.puts '<p>----- ----- ----- ----- ----- -----</p>'
+        end
+        puts "  有相关的新链接"+new_links.length.to_s+"条  "
+      else
+        puts "  没有新的链接 No news is good news "
       end
-      puts "  有相关的新链接"+new_links.length.to_s+"条  "
-    else
-      puts "  没有新的链接 No news is good news "
+      db.close if db
+      put_separator
+    rescue
+      puts "\n  数据库操作异常，请加参数 -c -i 重置数据库或联系开发人员"
+      browser.close
+      exit
     end
-    db.close if db
-    put_separator
   end
   
   def put_separator
@@ -105,7 +135,6 @@ class Graper
     puts "  ----- ----- ----- ----- ----- ----- ----- ----- ----- -----  "
     puts ""
   end
-  
 end
 
 options = {}
